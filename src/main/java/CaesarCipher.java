1c0d15695bd2954076b33850d6532d3cddfee00c
Chase (Ryan Cline) <ryancline88@yahoo.com>
2018-06-17 18:58:00 -0600

    created test and class template with empty methods

---
+++ src/main/java/CaesarCipher.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class CaesarCipher {
+    public static List<String> movingShift(String s, int shift) {
+
+    }
+
+    public static String demovingShift(List<String> s, int shift) {
+
+    }
+}
